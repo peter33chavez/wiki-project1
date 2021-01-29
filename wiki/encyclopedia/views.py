bad519968a3af2bc58cc3398279232fd41c2fb06
Peter Chavez <peter33chavez@gmail.com>
2021-01-28 19:27:48 -0800

    added pageError.html and fixed entry.html

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -9,14 +9,14 @@ def index(request):
     })
 
 def entry(request, title):
-    # if statement checking if the entry after wiki/ is a valid wiki page.
-        entries = util.list_entries()
-        for entry in entries:
-            if title == entry:
-            # render entry template with given info from the requested entry.
-                return render(request, "encyclopedia/entry.html", {
-                    "entry": entry
-                })
-    # else statement if page doesn't exist     
-            else:
-                return render(request, "encyclopedia/pageError.html")
+    #check if the title after "wiki/" is a valid entry.
+    entry = util.get_entry(title)
+    if entry is not None:
+    # render entry template with given info from the requested entry.
+        return render(request, "encyclopedia/entry.html", {
+            "entry": entry,
+            "title": title
+        })
+# else statement if page doesn't exist     
+    else:
+        return render(request, "encyclopedia/pageError.html")
