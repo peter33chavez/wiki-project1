6f9fccd47a3be1a634812dc81dacc87790a7ff96
Peter Chavez <peter33chavez@gmail.com>
2021-01-28 18:15:25 -0800

    implemented entry into views, urls, and created entry.html

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    # if statement checking if the entry after wiki/ is a valid wiki page.
+        entries = util.list_entries()
+        for entry in entries:
+            if title == entry:
+            # render entry template with given info from the requested entry.
+                return render(request, "encyclopedia/entry.html", {
+                    "entry": entry
+                })
+    # else statement if page doesn't exist     
+            else:
+                return render(request, "encyclopedia/pageError.html")
