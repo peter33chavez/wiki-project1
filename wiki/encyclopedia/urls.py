8fb7fdb8a480cbaf0d1697f76d31a2722b6283e4
Peter Chavez <peter33chavez@gmail.com>
2021-01-31 19:34:16 -0800

    search in views.py complete

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/search", views.search, name="search"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
@@ -9,7 +10,7 @@ def index(request):
     })
 
 def entry(request, title):
-    #check if the title string is a valid wiki entry.
+    # check if the title string is a valid wiki entry.
     entry = util.get_entry(title)
     if entry is not None:
         return render(request, "encyclopedia/entry.html", {
@@ -19,4 +20,31 @@ def entry(request, title):
     # render error if no wiki page exists    
     else:
         return render(request, "encyclopedia/pageError.html")
+
+def search(request):
+    if request.method == "POST":
+    # entry- what the user types into the search form
+        if request.POST.get("q"):
+            substring = False
+        # get for data 
+            searchItem = request.POST.get["q"]
+            entry = util.get_entry(searchItem)
+            # if searchItem isn't an exact match check for substring matches
+            if entry is None:
+                results = []
+                for title in util.list_entries():
+                    if title.find(searchItem) == -1:
+                        continue
+                    else:
+                        results.append(title)
+                if results:
+                    substring = True  
+
+            #return values from search
+            return render(request, "encyclopedia/search.html", {
+                "entry": entry,
+                "substring": substring,
+                "results": results
+            })
+
 
